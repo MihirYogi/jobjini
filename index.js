df28541a8c7dc63d3ea789cb4863e9167fba31d8
Mihir <mihiryogip@gmail.com>
2023-07-16 12:05:00 +0530

    Initial comit

---
+++ index.js
@@ -0,0 +1,114 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId)
+
+        // Validate that all variables exist
+        if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener('click', () => {
+                // show navbar
+                nav.classList.toggle('show')
+                // change icon
+                toggle.classList.toggle('bx-x')
+                // add padding to body
+                bodypd.classList.toggle('body-pd')
+                // add padding to header
+                headerpd.classList.toggle('body-pd')
+            })
+        }
+    }
+
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+
+    function colorLink() {
+        if (linkColor) {
+            linkColor.forEach(l => l.classList.remove('active'))
+            this.classList.add('active')
+        }
+    }
+    linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+    // Your code to run since DOM is loaded and ready
+});
+
+
+
+
+
+
+const charts = document.querySelectorAll(".users");
+
+charts.forEach(function (chart) {
+    var ctx = chart.getContext("2d");
+    var myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: ["Year 2018", "Year 2019", "Year 2020", "Year 2021", "Year 2022", "Year 2023"],
+            datasets: [
+                {
+                    label: "Users On Year",
+                    data: [50, 100, 200, 300, 400, 500],
+                    backgroundColor: [
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(54, 162, 235, 0.2)",
+                        "rgba(255, 206, 86, 0.2)",
+                        "rgba(75, 192, 192, 0.2)",
+                        "rgba(153, 102, 255, 0.2)",
+                        "rgba(255, 159, 64, 0.2)",
+                    ],
+                    borderColor: [
+                        "rgba(255, 99, 132, 1)",
+                        "rgba(54, 162, 235, 1)",
+                        "rgba(255, 206, 86, 1)",
+                        "rgba(75, 192, 192, 1)",
+                        "rgba(153, 102, 255, 1)",
+                        "rgba(255, 159, 64, 1)",
+                    ],
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+});
+
+
+
+let ctx = document.getElementById("myChart").getContext("2d");
+    let myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: [
+                "Year 2018",
+                "Year 2019",
+                "Year 2020",
+                "Year 2021",
+                "Year 2022",
+                "Year 2023",
+            ],
+            datasets: [
+                {
+                    label: "Total Jobs",
+                    data: [50, 200, 150, 300, 450, 500],
+                    backgroundColor: "lightblue",
+                },
+                {
+                    label: "Applied Jobs",
+                    data: [40, 100, 200, 250, 300, 400],
+                    backgroundColor: "blue",
+                },
+            ],
+        },
+    });
